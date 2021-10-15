fa7c2695ea5cc7d320a92c4995b4cc49607efe64
masa1203 <msys.sawada@gmail.com>
2021-10-15 23:45:31 +0900

    useEffectの諸語

--- react-demo/src/App.tsx
+++ react-demo/src/App.tsx
@@ -1,18 +1,36 @@
-import React, { useState } from "react";
+import { withWidth } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [title, setTitle] = useState("Practical React Enterprise!");
+  const handleAlert = () => {
+    alert("これはボタンです！");
+  };
+
+  // titleの変更をトリガーにしてeffectが起動する
+  useEffect(() => {
+    alert("Welcome to Practical React Enterprise");
+  }, [title]);
+
   return (
     <div className="App">
-      <h1
-        onClick={() =>
-          setTitle("Become an Effective React developer from Day1")
-        }
-        style={{ color: "blue", marginBottom: "5pt" }}
-      >
+      <h1 onClick={() => setTitle("Become an Effective React Developer")}>
         {title}
       </h1>
+      <button
+        style={{
+          color: "#ffff",
+          backgroundColor: "tomato",
+          height: "5rem",
+          width: "10rem",
+          fontSize: "18px",
+          borderRadius: "5px",
+        }}
+        onClick={handleAlert}
+      >
+        Click me
+      </button>
     </div>
   );
 }
