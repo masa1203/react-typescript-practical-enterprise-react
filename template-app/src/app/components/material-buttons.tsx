fb52978e3278f014add7454262fc6d276141d10d
masa1203 <msys.sawada@gmail.com>
2021-10-16 01:03:21 +0900

    boilertemplateからプロジェクトを開始して少し編集した

---
+++ template-app/src/app/components/material-buttons.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+function MaterialButtons() {
+  return (
+    <div>
+      <Button variant="contained"> Test </Button>
+      <Button variant="contained"> Test </Button>
+      <Button variant="contained"> Test </Button>
+      <Button variant="contained"> Test </Button>
+      <Button variant="contained"> Test </Button>
+    </div>
+  );
+}
+
+export default MaterialButtons;
